35bfca96d5261d1278cbf65bf2a64eae722c05d4
Justin Blau <justin.blau@oracle.com>
2020-03-02 17:16:32 -0500

    as

--- iftop/iftop.go
+++ iftop/iftop.go
@@ -13,6 +13,7 @@ import (
 type IftopStatus struct {
 	Status            string
 	BytesReceivedRate float64
+	TotalReceived     float64
 }
 
 var Status IftopStatus
@@ -66,38 +67,79 @@ func updateStatus(r io.Reader) {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		txt := scanner.Text()
-		res := strings.Index(txt, "Total receive rate:")
-		if res == -1 {
-			continue
+		updateBytesReceivedRate(txt)
+		updateTotalReceived(txt)
+	}
+}
+
+func updateBytesReceivedRate(txt string) {
+	res := strings.Index(txt, "Total receive rate:")
+	if res == -1 {
+		return
+	}
+	sub1 := txt[res:len(txt)]
+	res = strings.Index(sub1, "b")
+	sub2 := sub1[res+1 : len(sub1)]
+	res = strings.Index(sub2, "b")
+	sub3 := sub2[0:res]
+	sub3 = strings.TrimSpace(sub3)
+	size := string(sub3[len(sub3)-1])
+	if size == "K" {
+		v, err := strconv.ParseFloat(sub3[0:len(sub3)-1], 64)
+		if err != nil {
+			fmt.Println("Update status: " + err.Error())
 		}
-		sub1 := txt[res:len(txt)]
-		res = strings.Index(sub1, "b")
-		sub2 := sub1[res+1 : len(sub1)]
-		res = strings.Index(sub2, "b")
-		sub3 := sub2[0:res]
-		sub3 = strings.TrimSpace(sub3)
-		size := string(sub3[len(sub3)-1])
-		if size == "K" {
-			v, err := strconv.ParseFloat(sub3[0:len(sub3)-1], 64)
-			if err != nil {
-				fmt.Println("Update status: " + err.Error())
-			}
-			v = v * 1000
-			Status.BytesReceivedRate = v
-		} else if size == "M" {
-			v, err := strconv.ParseFloat(sub3[0:len(sub3)-1], 64)
-			if err != nil {
-				fmt.Println("Update status: " + err.Error())
-			}
-			v = v * 1000000
-			Status.BytesReceivedRate = v
-		} else if size == "G" {
-			v, err := strconv.ParseFloat(sub3[0:len(sub3)-1], 64)
-			if err != nil {
-				fmt.Println("Update status: " + err.Error())
-			}
-			v = v * 1000000000
-			Status.BytesReceivedRate = v
+		v = v * 1000
+		Status.BytesReceivedRate = v
+	} else if size == "M" {
+		v, err := strconv.ParseFloat(sub3[0:len(sub3)-1], 64)
+		if err != nil {
+			fmt.Println("Update status: " + err.Error())
 		}
+		v = v * 1000000
+		Status.BytesReceivedRate = v
+	} else if size == "G" {
+		v, err := strconv.ParseFloat(sub3[0:len(sub3)-1], 64)
+		if err != nil {
+			fmt.Println("Update status: " + err.Error())
+		}
+		v = v * 1000000000
+		Status.BytesReceivedRate = v
+	}
+}
+
+func updateTotalReceived(txt string) {
+	res := strings.Index(txt, "Cumulative (sent/received/total):")
+	if res == -1 {
+		return
+	}
+	sub1 := txt[res:len(txt)]
+	res = strings.Index(sub1, "B")
+	sub2 := sub1[res+1 : len(sub1)]
+	res = strings.Index(sub2, "B")
+	sub3 := sub2[0:res]
+	sub3 = strings.TrimSpace(sub3)
+	size := string(sub3[len(sub3)-1])
+	if size == "K" {
+		v, err := strconv.ParseFloat(sub3[0:len(sub3)-1], 64)
+		if err != nil {
+			fmt.Println("Update status: " + err.Error())
+		}
+		v = v * 1000
+		Status.TotalReceived = v
+	} else if size == "M" {
+		v, err := strconv.ParseFloat(sub3[0:len(sub3)-1], 64)
+		if err != nil {
+			fmt.Println("Update status: " + err.Error())
+		}
+		v = v * 1000000
+		Status.TotalReceived = v
+	} else if size == "G" {
+		v, err := strconv.ParseFloat(sub3[0:len(sub3)-1], 64)
+		if err != nil {
+			fmt.Println("Update status: " + err.Error())
+		}
+		v = v * 1000000000
+		Status.TotalReceived = v
 	}
 }
